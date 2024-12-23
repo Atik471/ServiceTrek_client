f7f9eb7b55ecb407194820e021c6f81b30660949
Atik471 <108218772+Atik471@users.noreply.github.com>
2024-12-23 21:13:10 +0600

    added get services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,14 +1,83 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
+import {LocationContext} from "../contexts/LocationProvider";
 
 const Services = () => {
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const serverDomain = useContext(LocationContext);
+
+  const limit = 10;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `${serverDomain}/services?page=${page}&limit=${limit}`
+        );
+        setItems(response.data.items);
+        setTotalPages(response.data.totalPages);
+        console.log(response.data);
+      } catch (err) {
+        console.error("Error fetching data:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [page]);
+
+  const handlePrev = () => {
+    if (page > 1) setPage((prev) => prev - 1);
+  };
+
+  const handleNext = () => {
+    if (page < totalPages) setPage((prev) => prev + 1);
+  };
+
+  if (loading) {
     return (
-        <div>
-            <Helmet>
-                <title>ServiceTrek | Services</title>
-            </Helmet>
-            Services
+      <div className="flex justify-center items-center h-screen">
+        <div className="relative">
+          <div className="w-28 h-28 border-8 border-primary border-solid rounded-full animate-spin border-t-transparent"></div>
+          <p className="absolute inset-0 flex items-center justify-center text-primary font-semibold text-xl">
+            Loading...
+          </p>
         </div>
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <Helmet>
+        <title>ServiceTrek | Services</title>
+      </Helmet>
+      Services
+      <h1>Paginated Items</h1>
+      <ul>
+        {items?.map((item) => (
+          <li key={item._id}>{item.title}</li>
+        ))}
+      </ul>
+      <div>
+        <button onClick={handlePrev} disabled={page === 1}>
+          Previous
+        </button>
+        <span>
+          Page {page} of {totalPages}
+        </span>
+        <button onClick={handleNext} disabled={page === totalPages}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
